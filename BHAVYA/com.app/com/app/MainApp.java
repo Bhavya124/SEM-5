89c879e4b276b9ff97900558abcc1ea27b8c41be
Bhavya 124 <bhavyasingla196@gmail.com>
2025-07-08 17:26:04 +0530

    8th july work done

---
+++ BHAVYA/com.app/com/app/MainApp.java
@@ -0,0 +1,8 @@
+package com.app;
+import com.greeting.Greeting;
+public class MainApp {
+    public static void main(String[] args) {
+        Greeting obj = new Greeting();
+        obj.sayHello();
+    }
+}
---
+++ BHAVYA/com.greeting/com/greeting/Greeting.java
@@ -0,0 +1,7 @@
+package com.greeting;
+
+public class Greeting {
+    public static void sayHello() {
+        System.out.println("HELLO GOOD AFTER EVE");
+    }
+}
