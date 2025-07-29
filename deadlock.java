81e324cac0bd22aedddee7131efe3d19adf79717
Bhavya 124 <bhavyasingla196@gmail.com>
2025-07-29 16:31:02 +0530

    29 jult work done

---
+++ deadlock.java
@@ -0,0 +1,11 @@
+public class deadlock {
+    public void fun(){
+        synchronized (Test.obj1){
+            synchronized(Test.obj2){}
+        }   
+    }
+    
+    public void fun2(){
+        synchronized Test.obj2;
+    }
+}
